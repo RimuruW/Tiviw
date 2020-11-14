28c6fb433e7d46348caac98f0ec33294b82beaba
QingxuMo <ruomo85@gmail.com>
2020-11-14 23:19:09 +0800

    优化安装脚本

--- install.sh
+++ install.sh
@@ -1,21 +1,39 @@
 #!/data/data/com.termux/files/usr/bin/bash
 if [ -t 1 ]; then
-	red=$(printf	'\033[31m')
-	green=$(printf	'\033[32m')
-	yellow=$(printf '\033[33m')
-	blue=$(printf	'\033[34m')
-	light_cyan=$(printf	'\033[1;96m')
-	reset=$(printf	'\033[0m')
+	RED=$(printf	'\033[31m')
+	GREEN=$(printf	'\033[32m')
+	YELLOW=$(printf '\033[33m')
+	BLUE=$(printf	'\033[34m')
+	LIGHT=$(printf	'\033[1;96m')
+	RESET=$(printf	'\033[0m')
 else
-	red=""
-	green=""
-	yellow=""
-	blue=""
-	light_cyan=""
-	reset=""
+	RED=""
+	GREEN=""
+	YELLOW=""
+	BLUE=""
+	LIGHT=""
+	RESET=""
 fi
 
+red() {
+	echo -e "${RED}$1${RESET}"
+}
 
+green() {
+	echo -e "${GREEN}$1${RESET}"
+}
+
+yellow() {
+	echo -e "${YELLOW}$1${RESET}"
+}
+
+blue() {
+	echo -e "${BLUE}$1${RESET}"
+}
+
+light() {
+	echo -e "${LIGHT}$1${RESET}"
+}
 function ask() {
     # http://djm.me/ask
     while true; do
@@ -32,7 +50,8 @@ function ask() {
         fi
 
         # Ask the question
-        printf '%s' "${light_cyan}[?]"
+		printf '%s\n' "${LIGHT}"
+        printf "[?] "
         read -r -p "$1 [$prompt] " REPLY
 
         # Default?
@@ -40,7 +59,7 @@ function ask() {
             REPLY=$default
         fi
 
-		printf '%s\n' "${reset}"
+		printf '%s\n' "${RESET}"
 
         # Check if the reply is valid
         case "$REPLY" in
@@ -52,60 +71,49 @@ function ask() {
 
 # Check
 if [[ $EUID -eq 0 ]]; then
-		printf  '%s\n' "${yellow}"
-	cat <<-'EOF'
-[!] 检测到您正在尝试使用 ROOT 权限运行安装脚本
-[!] 这是不建议且不被允许的
-[!] 安装全过程不需要 ROOT 权限,且以 ROOT 权限运行可能会带来一些无法预料的问题
-[!] 为了您的设备安全，请避免在任何情况下以 ROOT 用户运行安装脚本
-	EOF
-	printf '%s\n' "${reset}"
+	yellow "
+	[!] 检测到您正在尝试使用 ROOT 权限运行安装脚本
+	[!] 这是不建议且不被允许的
+	[!] 安装全过程不需要 ROOT 权限,且以 ROOT 权限运行可能会带来一些无法预料的问题
+	[!] 为了您的设备安全，请避免在任何情况下以 ROOT 用户运行安装脚本
+	"
 	exit 1
 fi
 if [[ -d /system/app && -d /system/priv-app ]]; then
 	systeminfo="Android $(getprop ro.build.version.release)"
 	export systeminfo
-else
-	printf  '%s\n' "${red}" 
-	printf	"[!] This operating system is not supported.\n"
-	printf	'%s\n' "${reset}"
+else 
+	red	"[!] This operating system is not supported."
 	exit 1
 fi
 
-if [ -d "$PREFIX/etc/tiviw" ]; then
-	printf  '%s' "${yellow}" 
-	printf	'%s\n' "[!] 您已安装 Tiviw ，无需重复安装" "如果您需要移除 Tiviw，请输入 rm -rf $PREFIX/etc/tiviw"
-	printf	'%s' "${reset}"
+if [ -d "$PREFIX/etc/tiviw" ]; then 
+	yellow "
+	[!] 您已安装 Tiviw ，无需重复安装
+	如果您需要移除 Tiviw，请输入 rm -rf $PREFIX/etc/tiviw
+	"
 	exit 1
 fi
 
 check_mirrors() {
 	mirrors_status=$(grep "mirror" "$PREFIX/etc/apt/sources.list" | grep -v '#')
-	if [ -z "$mirrors_status" ]; then
-		printf '%s' "${red}" 
-		printf "[!] Termux 镜像源未配置!"
-		printf '%s' "${reset}"
-		printf '%s\n' "${blue}" 
-		printf "对于国内用户，添加清华源作为镜像源可以有效增强 Termux 软件包下载速度\n" 
-		printf '%s\n' "${reset}"
+	if [ -z "$mirrors_status" ]; then 
+		red "[!] Termux 镜像源未配置!"
+		blue "对于国内用户，添加清华源作为镜像源可以有效增强 Termux 软件包下载速度" 
 		if ask "是否添加清华源?" "N"; then
 				sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' "$PREFIX/etc/apt/sources.list"
 				sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@'"$PREFIX/etc/apt/sources.list.d/game.list"
 				sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' "$PREFIX/etc/apt/sources.list.d/science.list"
 				apt update && apt upgrade -y
 			else
-				printf '%s' "${blue}"
-				printf "使用默认源进行安装\n"
-				printf '%s' "${reset}"
+				blue "使用默认源进行安装\n"
 		fi
 	fi
 }
 
 check_mirrors
 
-printf  '%s' "${blue}"
-printf	"[*] 检查依赖中…\n"
-printf	'%s' "${reset}"
+blue "[*] 检查依赖中…"
 apt-get update -y &> /dev/null
 for i in git wget; do
 if [ -e "$PREFIX/bin/$i" ]; then
@@ -113,64 +121,53 @@ if [ -e "$PREFIX/bin/$i" ]; then
 else
 	echo  "Installing $i..."
 	apt-get install -y $i || {
-		printf  '%s' "${red}"
-			printf	"[!] 依赖安装失败!\n[*] 退出中……\n"
-			printf	'%s' "${reset}"
+		red "
+			[!] 依赖安装失败!
+			[*] 退出中……
+			"
 			exit 1
 		}
 fi
 done
 apt-get upgrade -y
 
-printf  '%s' "${blue}"
-printf	"[*] 正在创建工作目录…\n"
-printf	'%s' "${reset}"
+blue "[*] 正在创建工作目录…\n"
 mkdir -p "$PREFIX/etc/tiviw"
 mkdir -p "$PREFIX/etc/tiviw/logs" "$PREFIX/etc/tiviw/linux" "$PREFIX/etc/tiviw/etc"
 
-printf  '%s' "${blue}"
-printf	"[*] 正在拉取远程仓库…\n"
-printf	'%s' "${reset}"
+blue "[*] 正在拉取远程仓库…\n"
 git clone https://github.com.cnpmjs.org/QingxuMo/Tiviw "$PREFIX/etc/tiviw/main"
-
-printf  '%s' "${blue}" 
-printf	"[*] 正在修改远程仓库地址…\n"
-printf	'%s' "${reset}"
-cd "$PREFIX/etc/tiviw/main" || { echo "${red}目录跳转失败！${reset}" >&2;  exit 1; }
+ 
+blue "[*] 正在修改远程仓库地址…\n"
+cd "$PREFIX/etc/tiviw/main" || { red "目录跳转失败！" >&2;  exit 1; }
 git remote set-url origin https://github.com/QingxuMo/Tiviw
 
-printf	'%s' "${blue}" 
-printf	"[*] 正在检查远程仓库地址…\n"
-printf	'%s' "${reset}"
+red	"[*] 正在检查远程仓库地址…\n"
 remote_status="$(git remote -v | grep "https://github.com/QingxuMo/Tiviw")"
-[[ -z "$remote_status" ]] && printf '%s\n' "${red}[!] 远程仓库地址修改失败!" "[!] 请提交错误内容至开发者!${reset} " && printf "\n"
+[[ -z "$remote_status" ]] && red "
+[!] 远程仓库地址修改失败!
+[!] 请提交错误内容至开发者!
+"
 
-printf  '%s' "${blue}"
-printf	"[*] 正在校验其他选项…\n"
-printf	'%s' "${reset}"
+blue "[*] 正在校验其他选项…\n"
 
-cd "$HOME" || { echo "${red}[!] 目录跳转失败!${reset}" >&2;  exit 1; }
+cd "$HOME" || { red "[!] 目录跳转失败!" >&2;  exit 1; }
 
-printf  '%s' "${blue}"
-printf	"[*] 正在修改文件权限…\n"
-printf	'%s' "${reset}"
+blue "[*] 正在修改文件权限…\n"
 bash "$PREFIX/etc/tiviw/main/permission.sh"
 
-printf  '%s' "${blue}"
-printf	"[*] 正在创建启动器…\n"
-printf	'%s' "${blue}"
+blue	"[*] 正在创建启动器…\n"
 rm -f "$PREFIX/bin/tiviw"
 cp "$PREFIX/etc/tiviw/main/tiviw" "$PREFIX/bin/tiviw"
 chmod +x "$PREFIX/bin/tiviw"
 
 if [ -f "$PREFIX/bin/tiviw" ]; then
-	printf  '%s' "${green}" 
-	printf	"[√]  安装成功！请输入 tiviw 启动脚本！"
-	printf	'%s' "${reset}"
+	green	"[√]  安装成功！请输入 tiviw 启动脚本！"
 	exit 0
 else
-	printf  '%s' "${red}"
-	printf	"[!] 安装失败！请提交错误内容至开发者！\n[*] 错误：启动器安装失败"
-	printf	'%s' "${reset}"
+	red	"
+	[!] 安装失败！请提交错误内容至开发者！
+	[*] 错误：启动器安装失败
+	"
 	exit 1
 fi
